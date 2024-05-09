76108108b2357499357010ad2a4c1950914fad95
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-05-09 19:55:31 -0300

    Creación del WebServer dentro del index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+// Importando el framework Express para utilizarlo dentro del archivo principal
+import express from "express";
+
+// Creando el 'web server' basado en Express
+const app = express();
+const PORT = 8080;
+
+// Middleware para trabajar con el protocolo HTTP; nos permite enviar información a través del 'body' de la petición
+app.use(express.json());
+// Middleware para trabajar con datos enviados a través de URL
+app.use(express.urlencoded({extended: true}));
+// Si queremos renderizar al usuario
+
+// Diciéndole al servidor web que se encuentre a la escucha de peticiones HTTP sobre el puerto 8080 local
+app.listen(PORT, () => {
+  console.log("Server already running in port localhost :8080");
+});
+// Si hubiese algún problema al momento de correr el servidor web, enviamos un error por la consola
+app.on("error", (errorDetail) => {
+  console.error(
+    `An error has ocurred while setting up the web server: ${errorDetail}`
+  );
+});
