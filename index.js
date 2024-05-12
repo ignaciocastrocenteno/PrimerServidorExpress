abd03c95f33a7b1933c20ea11655f3c11f9b56f8
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-05-12 18:53:55 -0300

    Comentarios acerca de las middlewares

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-// Middleware para trabajar con el protocolo HTTP; nos permite enviar información a través del 'body' de la petición
+/* Middleware para trabajar con el protocolo HTTP; nos permite enviar información a través del 'body' de la petición
+- req.body */
 app.use(express.json());
-// Middleware para trabajar con datos enviados a través de URL
+// Middleware para trabajar con datos enviados a través de URL - req.params
 app.use(express.urlencoded({extended: true}));
+
+// Estableciendo las 'routes' que forman parte de la API
+/* app.use("/", productsRoutes); */
 
 // Diciéndole al servidor web que se encuentre a la escucha de peticiones HTTP sobre el puerto 8080 local
 app.listen(PORT, () => {
   console.log("Server already running in port localhost :8080");
 });
+
 // Si hubiese algún problema al momento de correr el servidor web, enviamos un error por la consola
 app.on("error", (errorDetail) => {
   console.error(
