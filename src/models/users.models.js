9e440ebd3c40e6bde4c535145088ec956bfac878
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-05-18 03:14:55 -0300

    Refactorización de los modelos de la entidad '/users' a clases ES6

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,7 +1,7 @@
 // En la última capa 'models', se maneja la lógica de negocio y es el lugar donde se encuentra la información buscada.
 // Aquí se encuentra el acceso hacia las bases de datos o la consulta hacia APIs externas
 
-const getUsers = async () => {
+/* const getUsers = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = response.json();
 
@@ -34,17 +34,61 @@ const createUser = async (userToAdd) => {
   let data = response.json();
 
   return data;
-};
+}; */
 
 /* const updateUserByID = async () => {};
 
 const deleteUserByID = async () => {}; */
 
 // Exportamos todos las funciones que serán invocadas desde 'services', entregando la información solicitada
-export default {
+/* export default {
   getUsers,
   getUserByID,
   createUser,
-  /*  updateUserByID,
-  deleteUserByID, */
-};
+  // updateUserByID,
+  // deleteUserByID,
+}; */
+
+// REFACTORIZACIÓN DE 'MODELS' A CLASES ES6
+export default class UserModel {
+  constructor() {}
+
+  getUsers = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = response.json();
+
+    return data;
+  };
+
+  getUserByID = async (id) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    const data = response.json();
+
+    return data;
+  };
+
+  createUser = async (userToAdd) => {
+    // console.log(userToAdd);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      body: JSON.stringify({
+        type: "creation-of-user",
+        userData: userToAdd,
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+
+    let data = response.json();
+
+    return data;
+  };
+
+  // updateUserByID = async () => {};
+
+  // deleteUserByID = async () => {};
+}
