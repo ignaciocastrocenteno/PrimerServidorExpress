cddc9341b37c8bfaa1b6d11d7d95d592464dbc61
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-05-18 03:35:06 -0300

    Refactorización de los servicios de la entidad '/users' a clases ES6

--- src/services/users.services.js
+++ src/services/users.services.js
@@ -1,11 +1,13 @@
 // Esta capa 'services' es más que nada un "pasamanos": no maneja requerimientos ni lógica de negocio
 // Importamos los modelos que vamos a necesitar para responderle a cada controlador
-import models from "../models/users.models.js";
+// import models from "../models/users.models.js";
+import UserModel from "../models/users.models.js";
 
 /* En el archivo 'services', cada función (que es un servicio individual), gestiona el retorno directamente de la información
 exigida por el controlador que lo invoca. */
 // En este punto, ya sólo nos manejamos con funciones. Normalmente mantenemos el nombre de la función por coherencia
-const getUsers = async () => {
+
+/* const getUsers = async () => {
   const users = await models.getUsers();
   return users;
 };
@@ -20,15 +22,43 @@ const createUser = async (userToAdd) => {
   return result;
 };
 
-/* const updateUserByID = async () => {};
+// const updateUserByID = async () => {};
 
-const deleteUserByID = async () => {}; */
+// const deleteUserByID = async () => {}; */
 
 // Exportamos todos las funciones que serán invocadas desde 'controllers' para cumplir con los requerimientos
-export default {
+/* export default {
   getUsers,
   getUserByID,
   createUser,
-  /*   updateUserByID,
-  deleteUserByID, */
-};
+  // updateUserByID,
+  // deleteUserByID,
+}; */
+
+// REFACTORIZACIÓN DE 'SERVICES' A CLASES ES6
+export default class UserServices {
+  #models;
+  constructor() {
+    // Instanciamos los modelos de la entidad '/users' para utilizar sus métodos
+    this.models = new UserModel();
+  }
+
+  getUsers = async () => {
+    const users = await models.getUsers();
+    return users;
+  };
+
+  getUserByID = async (id) => {
+    const user = await models.getUserByID(id);
+    return user;
+  };
+
+  createUser = async (userToAdd) => {
+    const result = await models.createUser(userToAdd);
+    return result;
+  };
+
+  // const updateUserByID = async () => {};
+
+  // const deleteUserByID = async () => {};
+}
