57e0df9d7aaa6e1d4053b5b4a75246b42364a712
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-05-28 09:35:53 -0300

    Aplicación del patrón de diseño DAO (creación de factoría y adición de modelos adicionales)

---
+++ src/models/DAO/AbstractFactory.js
@@ -0,0 +1,40 @@
+import UsersModelsMemory from "../Users/users.memory.models.js";
+import UsersModelsFs from "../Users/users.fs.models.js";
+
+/* La clase 'ModelFactory' aplica el patrón de diseño DAO (Data Access Object), para gestionar los diferentes medios
+de persistencia de datos que hay disponibles en la aplicación. Siempre que veamos este patrón aplicado a un
+proyecto, sabemos que vamos a estar trabajando con 2 o más fuentes de datos. */
+
+/*
+  Ventajas de implementar el patrón DAO en proyectos:
+    - Crear y configurar fácilmente nuevos tipos de persistencias para el proyecto; brinda mayor robutez al código, ya que
+    no tenemos que refactorizar el codebase, sólo necesitamos cambiar el 'import' de cada archivo 'services', correspondiente
+    a cada entidad. De ese modo, los servicios siempre van a hablar con la factoría y no con los 'models' concretos.
+
+    - Actualización de persistencias: si el patrón está implementado dentro del proyecto y necesitamos actualizar la base de
+    datos, esta tarea es mucho más sencilla. Únicamente deberíamos manipular la factoría y conectarla con el modelo
+    actualizado.
+*/
+export default class ModelFactory {
+  constructor() {}
+
+  /* La ejecución de la factoría la hacemos 'static' para no tener que instanciar la clase. Ejecutamos el método,
+  enviándole por parámetro el tipo de persistencia que vamos a utilizar (un String indicando su tipo) */
+  static get(type) {
+    switch (type) {
+      case "MEM":
+        console.log("Persistiendo en la memoria del servidor!");
+        return new UsersModelsMemory();
+      case "FS":
+        console.log("Persistiendo sobre FileSystem (FS)");
+        return new UsersModelsFs();
+      case "MONGO_DB":
+        console.log("Persistiendo en la memoria de MongoDB!");
+        return new ProductsModelMongo();
+      // En caso que el input no coincida con ninguna de las persistencias disponibles, usamos la persistencia 'default'.
+      default:
+        console.log("Persistiendo en la memoria default!");
+        return new ProductsModelMemory();
+    }
+  }
+}
--- src/services/users.services.js
+++ src/services/users.services.js
@@ -1,7 +1,12 @@
 // Esta capa 'services' es más que nada un "pasamanos": no maneja requerimientos ni lógica de negocio
 // Importamos los modelos que vamos a necesitar para responderle a cada controlador
-// import models from "../models/users.models.js";
-import UserModel from "../models/users.models.js";
+// import UserModel from "../models/Users/users.memory.models.js";
+// import UserModel from "../models/Users/users.fs.models.js";
+
+// En lugar de cambiar manualmente la importación de 'models', hablamos con la factoría
+import ModelFactory from "../models/DAO/AbstractFactory.js";
+// Importamos desde 'config.js' el tipo de persistencia elegida para trabajar. Esto para que sea más escalable
+import config from "../../config.js";
 
 /* En el archivo 'services', cada función (que es un servicio individual), gestiona el retorno directamente de la información
 exigida por el controlador que lo invoca. */
@@ -39,8 +44,8 @@ const createUser = async (userToAdd) => {
 export default class UserServices {
   #models;
   constructor() {
-    // Instanciamos los modelos de la entidad '/users' para utilizar sus métodos
-    this.#models = new UserModel();
+    // Instanciamos los modelos de la entidad '/users' para utilizar sus métodos, de acuerdo al tipo de persistencia elegida
+    this.#models = ModelFactory.get(config.PERSISTENCE_TYPE);
   }
 
   getUsers = async () => {
@@ -49,7 +54,7 @@ export default class UserServices {
   };
 
   getUserByID = async (id) => {
-    const user = await this.#models.getUserByID(id);
+    const user = await this.#models.getUsersByID(id);
     return user;
   };
 
