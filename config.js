49ffeca05951db7c7c44af4ead82db51b404b92b
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2024-07-01 19:07:40 -0300

    Agregando variables de entorno, en caso de querer utilizar MongoDB (con Mongo Atlas o localmente)

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const PORT = 8080;
 const PERSISTENCE_TYPE = "MEM";
+/* const CONNECTION_STRING = "mongodb+srv://admin:admin@clustertp2.zd4mpb5.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTP2";
+const DB_NAME = "tp2"; */
 
 export default {
   PORT,
